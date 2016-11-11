e2882c8282ea27a07aae83fa1f909dfcdfb9f5c1
zhengLi <a838502550@gmail.com>
2016-11-11 22:38:12 +0800

    开始设计第一个用户界面

--- GUI_test.py
+++ GUI_test.py
@@ -1,8 +1,10 @@
 # coding:utf-8
-# http://blog.csdn.net/chenghit/article/details/50421090
-# http://www.cnblogs.com/dyx1024/archive/2012/07/05/2578579.html
+# http://blog.csdn.net/chenghit/article/details/50421090  入门中文版
+# http://www.cnblogs.com/dyx1024/archive/2012/07/05/2578579.html xxx
 # http://blog.csdn.net/lyhdream/article/details/39702765
 # http://www.cnblogs.com/hester/p/4696519.html
+# http://blog.csdn.net/gzh0222/article/details/10376227
+# http://tool.oschina.net/commons?type=3 字体颜色RGB获取网站
 import wx
 
 
@@ -56,24 +58,29 @@ class RebuildFrame(wx.Frame):  # 主框体,所有界面都往Frame里加
 
     def confisrm_button(self, event):
         self.notebook.Show(True)
-        form1 = ATestPanel(self.notebook)
+        form1 = BaseInfoOfStudentPanel(self.notebook)
         form2 = ATestPanel2(self.notebook)
         form3 = ATestPanel3(self.notebook)
-        self.notebook.AddPage(form1, "test")
+        self.notebook.AddPage(form1, u"基本信息")
         self.notebook.AddPage(form2, "test1")
         self.notebook.AddPage(form3, "test2")
 
 
-class ATestPanel(wx.Panel):
+class BaseInfoOfStudentPanel(wx.Panel):
     def __init__(self,  *args, **kwargs):
-        super(ATestPanel, self).__init__(*args, **kwargs)
-        self.login_name_label = wx.StaticText(self, label=u"妈的.终于写完框架了-1", pos=(120, 160))
+        super(BaseInfoOfStudentPanel, self).__init__(*args, **kwargs)
+        font = wx.Font(18, wx.SWISS, wx.NORMAL, wx.BOLD)  # 设置字体
+
+        self.login_name_label = wx.StaticText(self, label=u"学生基本信息管理", pos=(308, 10))
+        self.login_name_label.SetFont(font)
+        self.login_name_label.SetForegroundColour("#21c4c3")  # 设置字体颜色
+
 
 
 class ATestPanel2(wx.Panel):
     def __init__(self,  *args, **kwargs):
         super(ATestPanel2, self).__init__(*args, **kwargs)
-        self.login_name_label = wx.StaticText(self, label=u"妈的.终于写完框架了-2", pos=(120, 160))
+        self.login_name_label = wx.StaticText(self, label=u"妈的.终于写完框架了-1", pos=(120, 160))
 
 
 class ATestPanel3(wx.Panel):
